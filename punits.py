57c367304cc8b576fbc3339eb9eef85310a8e3fe
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-11-21 17:38:36 +0000

    added multiplication functionality

--- punits.py
+++ punits.py
@@ -5,7 +5,8 @@ class punit(object):
 			self.unit=unit
 			self.import_config()
 			self.unit_conversion()
-			
+		else: self.unit={}
+
 	def import_config(self):
 		#import dictionary of prefixes and units
 		import yaml
@@ -19,7 +20,7 @@ class punit(object):
 				self.coeff*=self.prefixes_dict[key]
 				self.unit=self.unit.replace(key,'')
 				break
-				
+
 		#scan if unit is in unit dict, rename unit (SI) and adjust coefficient
 		found_match=False
 		for unit_type in self.units_dict:
@@ -27,10 +28,10 @@ class punit(object):
 				if self.unit == unit:
 					self.coeff*=self.units_dict[unit_type][unit]
 					self.unit=unit_type
-					found_match=True				
+					found_match=True
 		if not found_match:
 			raise ValueError("Unit not found")
-			
+
 		#transform string of self.unit into dictionary {SI:exponent}
 		import re
 		self.unit=self.unit.split(',') #split string into tokens
@@ -43,14 +44,30 @@ class punit(object):
 			unit_dict[unit.group()]=exponent #fill dictionary with SI units and exponents
 		self.unit=unit_dict
 
-			
 	def __eq__(self,other):
 		if type(other)==punit:
 			if self.coeff==other.coeff and self.unit==other.unit:
 				return True
 			else: return False
 		else: raise ValueError("can only compare punit with punit")
+
+	def __mul__(self,other):
+		if type(other)!=punit:
+			other=punit(other)
+		result=punit(1)
+		all_units=set(self.unit.keys()).union(set(other.unit.keys()))
+		common_units=set(self.unit.keys()).intersection(set(other.unit.keys()))
+		for key in all_units:
+			if key in common_units:
+				result.unit[key]=self.unit[key]+other.unit[key]
+			elif key in self.unit.keys():
+				result.unit[key]=self.unit[key]
+			elif key in other.unit.keys():
+				result.unit[key]=other.unit[key]
+		result.coeff=self.coeff*other.coeff
+		return result
 		
-	
-		
-a=punit(1,'joule')
+a=punit(2,'joules')
+b=punit(3,'seconds')
+print b.unit
+print a*b
